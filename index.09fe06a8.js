142d71a284ce68c8869528051db62f64d70e9696
MatiushechkoMark <mmatiushechko@gmail.com>
2024-06-11 14:06:54 +0300

    make build

---
+++ index.09fe06a8.js
@@ -0,0 +1,2 @@
+!function e(n,r){var t=document.createElement("ul");for(var a in r)if(r.hasOwnProperty(a)){var l=document.createElement("li");l.textContent=a,e(l,r[a]),t.appendChild(l)}t.children.length>0&&n.appendChild(t)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.09fe06a8.js.map
